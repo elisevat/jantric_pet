af6c404bcc17c9caab823c4d3ebdab1a5299c19f
Elis <elis_eva05@mail.ru>
2024-08-13 19:33:58 +0700

    Добавление в корзину при помощи ajax

--- tech_goods_store/carts/urls.py
+++ tech_goods_store/carts/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'carts'
 
 urlpatterns = [
-    path('add/<slug:product_slug>/', views.cart_add, name='cart_add'),
-    path('change/<slug:product_slug>/', views.cart_change, name='cart_change'),
-    path('remove/<int:cart_id>/', views.cart_remove, name='cart_remove')
+    path('add/', views.cart_add, name='cart_add'),
+    path('change/', views.cart_change, name='cart_change'),
+    path('remove/<int:cart_id>', views.cart_remove, name='cart_remove')
 ]
--- tech_goods_store/carts/views.py
+++ tech_goods_store/carts/views.py
@@ -1,13 +1,16 @@
+from django.http import JsonResponse
 from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
 
 from goods.models import Products
+from .utils import get_user_carts
 from .models import Cart
 
 # Create your views here.
 
-def cart_add(request, product_slug):
-    
-    product = Products.objects.get(slug=product_slug)
+def cart_add(request):
+    product_id = request.POST.get('product_id')
+    product = Products.objects.get(id=product_id)
     
     if request.user.is_authenticated:
         carts = Cart.objects.filter(product=product, user=request.user)
@@ -19,8 +22,17 @@ def cart_add(request, product_slug):
                 cart.save()
         else:
             Cart.objects.create(user=request.user, product=product)
+
+    user_cart = get_user_carts(request)
+
+    cart_items_html = render_to_string(
+        'carts/includes/included_cart_with_buttom.html', {'carts': user_cart}, request=request)
     
-    return redirect(request.META['HTTP_REFERER'])
+    response_data = {
+        'message': 'Товар добавлен в корзину',
+        'cart_items_html': cart_items_html
+    }
+    return JsonResponse(response_data)
 
 
 def cart_change(request, product_slug):
